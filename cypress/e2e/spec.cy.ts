cb5bd0699ca0e08c6344b1e8bfd72c19cd08a723
James Spears <james.w.spears@gmail.com>
2023-04-21 11:06:24 -0400

    increase wait

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200');
-    cy.wait(15000);
+    cy.wait(30000);
     cy.get('[data-test-id="value"]').contains('Found 0 items in the DB.');
   });
 });
