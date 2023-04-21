09833caddd4c0398e5255326584e2e141d8c5500
James Spears <james.w.spears@gmail.com>
2023-04-21 10:59:48 -0400

    add seed script

---
+++ lambdas/seed.mjs
@@ -0,0 +1,42 @@
+import { DynamoDBClient, CreateTableCommand } from '@aws-sdk/client-dynamodb';
+
+const configuration = {
+  apiVersion: '2012-08-10',
+  endpoint: 'http://localhost:8000'
+}
+
+const ddbClient = new DynamoDBClient(configuration);
+
+const params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: 'id',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'id',
+      KeyType: 'HASH'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  },
+  TableName: 'SampleTable',
+  StreamSpecification: {
+    StreamEnabled: false
+  }
+};
+
+const run = async () => {
+  try {
+    const table = await ddbClient.send(new CreateTableCommand(params));
+    return table;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+run();
